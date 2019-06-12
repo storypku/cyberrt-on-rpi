39107bfc11fb4366281d57bb2b24c65948fef1dc
Liu Jiaming <storypku@gmail.com>
2019-06-13 04:42:21 +0800

    cross platform done by shell script

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 - Bazel version was 0.16.1@PX2, 0.18.0/rPi3
 - Experiment carried out mainly on one PX2, Nvidia
 
+## How to start
+```
+bash cyber_l10n.sh
+```
+
 ## Known problems
 - AtomicHashMap isn't thread-safe.
 - ConcurrentObjectPool sacrifices effeciency for not so commonly used `size()` functionality. Worth or Not?
---
+++ cyber_l10n.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+which pkg-config &>/dev/null
+if [ $? -ne 0 ]; then
+    echo "===== Oops! Command \"pkg-config\" not found. ====="
+    echo "  -> Please install it first!"
+    echo "  -> This is usually done by something like \"sudo apt install pkg-config\""
+    echo
+    exit 1
+fi
+
+zlib_dir=$(pkg-config --variable=libdir zlib 2>/dev/null)
+if [ $? -ne 0 ]; then
+    echo "===== Oops! No library libz.so can be found by pkg-config. ====="
+    exit 1
+fi
+
+sed "s#ZLIB_PATH#${zlib_dir}#g" WORKSPACE.in > WORKSPACE
+
+
