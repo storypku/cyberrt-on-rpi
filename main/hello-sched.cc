3f215bc7cef22cc1cddfbdec8394beba900ae594
Liu Jiaming <storypku@gmail.com>
2019-07-03 20:32:00 +0800

    croutine:scheduler:task w/o data_visitor

--- main/hello-sched.cc
+++ main/hello-sched.cc
@@ -1,8 +1,26 @@
 #include "cyber/common/log.h"
 #include "cyber/scheduler/scheduler_factory.h"
+#include "cyber/croutine/croutine.h"
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
 
 using GlobalData = apollo::cyber::common::GlobalData;
+using CRoutine = apollo::cyber::croutine::CRoutine;
+using RoutineState = apollo::cyber::croutine::RoutineState;
 using SchedulerChoreography = apollo::cyber::scheduler::SchedulerChoreography;
+
+#define EXPECT_EQ(a, b, msg)    \
+if ((a) != (b)) {               \
+    LOG(ERROR) << "Left: " << (a) << ", Right: " << (b) << ": " << msg; \
+                exit(1); }
+
+void monitor_func() {
+    LOG(INFO) << "MonitorFunc started";
+    CRoutine::GetCurrentRoutine()->HangUp();
+    // CRoutine::GetCurrentRoutine()->Sleep(std::chrono::microseconds(50));
+    LOG(INFO) << "MonitorFunc finished";
+}
 
 int main(int argc, char *argv[]) {
     google::InitGoogleLogging(argv[0]);
@@ -12,5 +30,16 @@ int main(int argc, char *argv[]) {
         LOG(ERROR) << "Failed to create SchedulerChoreography.";
         return -1;
     }
+
+    auto cr1 = std::make_shared<CRoutine>(monitor_func);
+    auto cr1_id = GlobalData::RegisterTaskName("sched_monitor");
+    cr1->set_id(cr1_id);
+    cr1->set_name("sched_monitor");
+    bool success = sched->DispatchTask(cr1);
+    EXPECT_EQ(success, true, "DispatchTask failed");
+    std::this_thread::sleep_for(std::chrono::microseconds(50));
+    success = sched->NotifyTask(cr1_id);
+    EXPECT_EQ(success, true, "Notifyfailed");
+    std::cin.ignore();
     return 0;
 }
